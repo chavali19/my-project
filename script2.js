b7545d48350df0f2f4ff85999a011c98a7440edb
chavali19 <83065546+chavali19@users.noreply.github.com>
2021-06-15 13:07:28 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@
       var getamount=document.getElementById("amount").value;
      
       var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - getamount;
-      if(getamount>17000 || isNAN(getamount))
+      if(getamount>17000)
       {
       alert("Insufficient Balance.");
       }
