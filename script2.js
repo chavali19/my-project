363e8c72b1c8e0a4063c074636596c72881c03c2
chavali19 <83065546+chavali19@users.noreply.github.com>
2021-06-15 14:03:15 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,11 +7,11 @@
       {
       alert("Insufficient Balance.");
       }
-      if(!(Number.isInteger(getamount)))
+      if(isNaN(getamount))
      {
       alert("Enter Correct Amount");
      }
-      else{
+      else if(!isNAN(getamount)){
       document.getElementById("result").innerHTML="You have done transaction of amount "+getamount+"to  "+document.getElementById("names").value+" <br> Your Account Balance is   "+myAccountBalance;
        document.getElementById("myAccountBalance").innerText = myAccountBalance;
            var createPTag = document.createElement("li");
