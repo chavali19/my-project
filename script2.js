6615349c693d6480d2fbb263a978d09bf4780263
chavali19 <83065546+chavali19@users.noreply.github.com>
2021-06-15 13:00:24 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,6 +7,8 @@
       {
       alert("Insufficient Balance.");
       }
+     else if(!isNAN(getamount))
+      alert("Enter valid amount");
       else{
       document.getElementById("result").innerHTML="You have done transaction of amount "+getamount+"to  "+document.getElementById("names").value+" <br> Your Account Balance is   "+myAccountBalance;
        document.getElementById("myAccountBalance").innerText = myAccountBalance;
